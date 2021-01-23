56649b51c978bdfba4eafa97e53c98a6f32e92da
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-01-22 19:10:01 -0500

    Added JS code to ppair up with the buttons

--- hw.js
+++ hw.js
@@ -1,2 +1,23 @@
-var now = dayjs();
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+//Funciton is what creates functionality. Without it,
+//the website won't work.
+$(document).ready(function(){
+
+//listening for save button clicks
+$(".saveBtn").on("click", function(){
+    //We are going up in the DOM with this code here. This allows
+    //us to focus in on the saveBtn itself.
+    console.log("CLICKED", $(this).parent().attr("id"));
+    
+
+
+
+});
+
+
+
+
+
+
+
+    
+})
